b0e3baa8d502c7b886fabec767f15499b773278a
lvalido <lvalido@mailexternos.com.ar>
2017-04-06 14:46:09 -0300

    [Cidob] Upload IOC

---
+++ Cidob.IOC/ServiceLocatorFluent.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Autofac;
+
+namespace Cidob.IOC
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ServiceLocatorFluent
+    {
+        #region const        
+        /// <summary>
+        /// The assembly prefix name
+        /// </summary>
+        private const string AssemblyPrefixName = "mervalito";
+        #endregion
+        #region Public Properties        
+        /// <summary>
+        /// Gets or sets the container.
+        /// </summary>
+        /// <value>
+        /// The container.
+        /// </value>
+        static IContainer Container { get; set; }
+        #endregion
+
+        #region Private Properties        
+        /// <summary>
+        /// The builder
+        /// </summary>
+        protected ContainerBuilder Builder;
+        #endregion
+
+        #region Virtual Methods                
+        /// <summary>
+        /// Creates the container.
+        /// </summary>
+        /// <param name="loadedAsm">The loaded asm.</param>
+        /// <returns></returns>
+        public ServiceLocatorFluent CreateContainer()
+        {
+            LoadAssemblies();
+
+            var loadedAsm = GetLoadedAssemblies();
+
+            Builder = new ContainerBuilder();
+            Builder.RegisterAssemblyTypes(loadedAsm)
+                .AsImplementedInterfaces()
+                .AsSelf()
+                .InstancePerDependency().
+                PropertiesAutowired();
+            return this;
+        }
+        /// <summary>
+        /// Creates the container.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="config">The configuration.</param>
+        /// <returns></returns>
+        public ServiceLocatorFluent CreateContainer<T>(T config)
+        {
+            CreateContainer();
+
+            OnCreateContainer(Builder, GetLoadedAssemblies(), config);
+            return this;
+        }
+        /// <summary>
+        /// Registers the type.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="I"></typeparam>
+        /// <returns></returns>
+        public ServiceLocatorFluent Register<T>(T instance) where T : class
+        {
+            Builder.RegisterInstance(instance).AsImplementedInterfaces().AsSelf().As<T>();
+            return this;
+        }
+        /// <summary>
+        /// Builds this instance.
+        /// </summary>
+        public ServiceLocatorFluent Build()
+        {
+            Container = Builder.Build();
+            return this;
+        }
+        /// <summary>
+        /// Gets the raw container.
+        /// </summary>
+        /// <returns></returns>
+        public IContainer GetRawContainer()
+        {
+            return Container;
+        }
+
+        /// <summary>
+        /// Called when [create container].
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="builder">The builder.</param>
+        /// <param name="loadedAssemblies">The loaded assemblies.</param>
+        /// <param name="config">The configuration.</param>
+        protected virtual void OnCreateContainer<T>(ContainerBuilder builder, Assembly[] loadedAssemblies, T config)
+        {
+
+        }
+        #endregion
+
+        #region Private Methods        
+        /// <summary>
+        /// Gets the loaded assemblies.
+        /// </summary>
+        /// <returns></returns>
+        Assembly[] GetLoadedAssemblies() => AppDomain.CurrentDomain.GetAssemblies().Where(FilterDomainAssembly).ToArray();
+
+        /// <summary>
+        /// Loads the assemblies.
+        /// </summary>
+        void LoadAssemblies()
+        {
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                LoadReferencedAssembly(assembly);
+            }
+        }
+
+        /// <summary>
+        /// Loads the referenced assembly.
+        /// </summary>
+        /// <param name="assembly">The assembly.</param>
+        void LoadReferencedAssembly(Assembly assembly)
+        {
+            foreach (var name in assembly.GetReferencedAssemblies().Where(FilterDomainAssembly))
+            {
+                if (AppDomain.CurrentDomain.GetAssemblies().All(a => a.FullName != name.FullName))
+                {
+                    LoadReferencedAssembly(Assembly.Load(name));
+                }
+            }
+        }
+        /// <summary>
+        /// Filters the domain assembly.
+        /// </summary>
+        /// <param name="asm">The asm.</param>
+        /// <returns></returns>
+        public bool FilterDomainAssembly(AssemblyName asm)
+        {
+            return asm.FullName.ToLowerInvariant().Contains(AssemblyPrefixName);
+        }
+        /// <summary>
+        /// Filters the domain assembly.
+        /// </summary>
+        /// <param name="asm">The asm.</param>
+        /// <returns></returns>
+        static bool FilterDomainAssembly(Assembly asm)
+        {
+            return asm.FullName.ToLowerInvariant().Contains(AssemblyPrefixName);
+        }
+        #endregion
+
+        #region Public Methods        
+        /// <summary>
+        /// Resolves this instance.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public T Resolve<T>()
+        {
+            return Container.Resolve<T>();
+        }
+        #endregion
+    }
+}
---
+++ Cidob.IOC/ServiceLocatorWebFluent.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Web.Http;
+using Autofac;
+using Autofac.Integration.WebApi;
+using Cidob.IOC;
+
+namespace Cidob.IOC
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <seealso cref="ServiceLocatorFluent" />
+    public class ServiceLocatorWebFluent : ServiceLocatorFluent
+    {
+        #region Overrides of ServiceLocatorFluent
+
+        /// <summary>
+        /// Called when [create container].
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="builder">The builder.</param>
+        /// <param name="loadedAssemblies">The loaded assemblies.</param>
+        /// <param name="config">The configuration.</param>
+        protected override void OnCreateContainer<T>(ContainerBuilder builder, Assembly[] loadedAssemblies, T config)
+        {
+            var httpConfiguration = config as HttpConfiguration;
+            Builder.RegisterApiControllers(loadedAssemblies).PropertiesAutowired();
+            Builder.RegisterWebApiFilterProvider(httpConfiguration);
+        }
+
+        #endregion
+    }
+}
