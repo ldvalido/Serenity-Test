29897528d6e6d04d272075bd8a23a002a2e01da7
Leandro Valido <leandrovalido@gmail.com>
2019-03-16 20:34:28 -0300

    [Cidob] Relationship between fields - Script to initial Loads

---
+++ Scripts/13_FILL_Table_BaseCover.sql
@@ -0,0 +1,52 @@
+
+INSERT INTO Base_Cover SELECT (SELECT TOP 1  IdBase FROM  Base WHERE Description = 'Tradicional' And [Order]=1),  (SELECT IdCover FROM Cover WHERE Description = 'Cuero')
+INSERT INTO Base_Cover SELECT (SELECT TOP 1  IdBase FROM  Base WHERE Description = 'Microcelular sin Escátola' And [Order]=5),  (SELECT IdCover FROM Cover WHERE Description = 'Evazote')
+INSERT INTO Base_Cover SELECT (SELECT TOP 1  IdBase FROM  Base WHERE Description = 'Microcelular sin Escátola' And [Order]=5),  (SELECT IdCover FROM Cover WHERE Description = 'Evazote Microperforado')
+INSERT INTO Base_Cover SELECT (SELECT TOP 1  IdBase FROM  Base WHERE Description = 'Microcelular sin Escátola' And [Order]=5),  (SELECT IdCover FROM Cover WHERE Description = 'Plastazote')
+INSERT INTO Base_Cover SELECT (SELECT TOP 1  IdBase FROM  Base WHERE Description = 'Microcelular sin Escátola' And [Order]=5),  (SELECT IdCover FROM Cover WHERE Description = 'Plastazote Microperforado')
+INSERT INTO Base_Cover SELECT (SELECT TOP 1  IdBase FROM  Base WHERE Description = 'Microcelular sin Escátola' And [Order]=5),  (SELECT IdCover FROM Cover WHERE Description = 'Multiform')
+INSERT INTO Base_Cover SELECT (SELECT TOP 1  IdBase FROM  Base WHERE Description = 'Microcelular sin Escátola' And [Order]=5),  (SELECT IdCover FROM Cover WHERE Description = 'Neoprene')
+INSERT INTO Base_Cover SELECT (SELECT TOP 1  IdBase FROM  Base WHERE Description = 'Microcelular con Escátola' And [Order]=4),  (SELECT IdCover FROM Cover WHERE Description = 'Evazote')
+INSERT INTO Base_Cover SELECT (SELECT TOP 1  IdBase FROM  Base WHERE Description = 'Microcelular con Escátola' And [Order]=4),  (SELECT IdCover FROM Cover WHERE Description = 'Evazote Microperforado')
+INSERT INTO Base_Cover SELECT (SELECT TOP 1  IdBase FROM  Base WHERE Description = 'Microcelular con Escátola' And [Order]=4),  (SELECT IdCover FROM Cover WHERE Description = 'Plastazote')
+INSERT INTO Base_Cover SELECT (SELECT TOP 1  IdBase FROM  Base WHERE Description = 'Microcelular con Escátola' And [Order]=4),  (SELECT IdCover FROM Cover WHERE Description = 'Plastazote Microperforado')
+INSERT INTO Base_Cover SELECT (SELECT TOP 1  IdBase FROM  Base WHERE Description = 'Microcelular con Escátola' And [Order]=4),  (SELECT IdCover FROM Cover WHERE Description = 'Multiform')
+INSERT INTO Base_Cover SELECT (SELECT TOP 1  IdBase FROM  Base WHERE Description = 'Microcelular con Escátola' And [Order]=4),  (SELECT IdCover FROM Cover WHERE Description = 'Neoprene')
+INSERT INTO Base_Cover SELECT (SELECT TOP 1  IdBase FROM  Base WHERE Description = 'Gel Polímero sin Escátola' And [Order]=7),  (SELECT IdCover FROM Cover WHERE Description = 'Cuero')
+INSERT INTO Base_Cover SELECT (SELECT TOP 1  IdBase FROM  Base WHERE Description = 'Celuloide (Finita)' And [Order]=21),  (SELECT IdCover FROM Cover WHERE Description = 'Cuero')
+INSERT INTO Base_Cover SELECT (SELECT TOP 1  IdBase FROM  Base WHERE Description = 'BlueFlex s/escatola' And [Order]=29),  (SELECT IdCover FROM Cover WHERE Description = 'Evazote Micro Azul')
+INSERT INTO Base_Cover SELECT (SELECT TOP 1  IdBase FROM  Base WHERE Description = 'BlueFlex c/Esctola' And [Order]=30),  (SELECT IdCover FROM Cover WHERE Description = 'Evazote Micro Azul')
+INSERT INTO Base_Cover SELECT (SELECT TOP 1  IdBase FROM  Base WHERE Description = '3D' And [Order]=28),  (SELECT IdCover FROM Cover WHERE Description = 'Evazote')
+INSERT INTO Base_Cover SELECT (SELECT TOP 1  IdBase FROM  Base WHERE Description = '3D' And [Order]=28),  (SELECT IdCover FROM Cover WHERE Description = 'Evazote Microperforado')
+INSERT INTO Base_Cover SELECT (SELECT TOP 1  IdBase FROM  Base WHERE Description = '3D' And [Order]=28),  (SELECT IdCover FROM Cover WHERE Description = 'Plastazote')
+INSERT INTO Base_Cover SELECT (SELECT TOP 1  IdBase FROM  Base WHERE Description = '3D' And [Order]=28),  (SELECT IdCover FROM Cover WHERE Description = 'Plastazote Microperforado')
+INSERT INTO Base_Cover SELECT (SELECT TOP 1  IdBase FROM  Base WHERE Description = '3D' And [Order]=28),  (SELECT IdCover FROM Cover WHERE Description = 'Multiform')
+INSERT INTO Base_Cover SELECT (SELECT TOP 1  IdBase FROM  Base WHERE Description = 'Polifibra' And [Order]=23),  (SELECT IdCover FROM Cover WHERE Description = 'Evazote')
+INSERT INTO Base_Cover SELECT (SELECT TOP 1  IdBase FROM  Base WHERE Description = 'Polifibra' And [Order]=23),  (SELECT IdCover FROM Cover WHERE Description = 'Evazote Microperforado')
+INSERT INTO Base_Cover SELECT (SELECT TOP 1  IdBase FROM  Base WHERE Description = 'Polifibra' And [Order]=23),  (SELECT IdCover FROM Cover WHERE Description = 'Plastazote')
+INSERT INTO Base_Cover SELECT (SELECT TOP 1  IdBase FROM  Base WHERE Description = 'Polifibra' And [Order]=23),  (SELECT IdCover FROM Cover WHERE Description = 'Plastazote Microperforado')
+INSERT INTO Base_Cover SELECT (SELECT TOP 1  IdBase FROM  Base WHERE Description = 'Polifibra' And [Order]=23),  (SELECT IdCover FROM Cover WHERE Description = 'Multiform')
+INSERT INTO Base_Cover SELECT (SELECT TOP 1  IdBase FROM  Base WHERE Description = 'PPS' And [Order]=22),  (SELECT IdCover FROM Cover WHERE Description = 'Plastazote')
+INSERT INTO Base_Cover SELECT (SELECT TOP 1  IdBase FROM  Base WHERE Description = 'Talon reforzado' And [Order]=2),  (SELECT IdCover FROM Cover WHERE Description = 'Cuero')
+INSERT INTO Base_Cover SELECT (SELECT TOP 1  IdBase FROM  Base WHERE Description = 'Talonera Móvil' And [Order]=24),  (SELECT IdCover FROM Cover WHERE Description = 'Cuero')
+INSERT INTO Base_Cover SELECT (SELECT TOP 1  IdBase FROM  Base WHERE Description = 'Termoplástica' And [Order]=25),  (SELECT IdCover FROM Cover WHERE Description = 'Evazote')
+INSERT INTO Base_Cover SELECT (SELECT TOP 1  IdBase FROM  Base WHERE Description = 'Termoplástica' And [Order]=25),  (SELECT IdCover FROM Cover WHERE Description = 'Evazote Microperforado')
+INSERT INTO Base_Cover SELECT (SELECT TOP 1  IdBase FROM  Base WHERE Description = 'Termoplástica' And [Order]=25),  (SELECT IdCover FROM Cover WHERE Description = 'Plastazote')
+INSERT INTO Base_Cover SELECT (SELECT TOP 1  IdBase FROM  Base WHERE Description = 'Termoplástica' And [Order]=25),  (SELECT IdCover FROM Cover WHERE Description = 'Plastazote Microperforado')
+INSERT INTO Base_Cover SELECT (SELECT TOP 1  IdBase FROM  Base WHERE Description = 'Termoplástica' And [Order]=25),  (SELECT IdCover FROM Cover WHERE Description = 'Multiform')
+INSERT INTO Base_Cover SELECT (SELECT TOP 1  IdBase FROM  Base WHERE Description = 'Talonera Gel Polimero' And [Order]=26),  (SELECT IdCover FROM Cover WHERE Description = 'Cuero')
+INSERT INTO Base_Cover SELECT (SELECT TOP 1  IdBase FROM  Base WHERE Description = 'Triple Densidad' And [Order]=31),  (SELECT IdCover FROM Cover WHERE Description = 'Plastazote')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
